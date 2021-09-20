f69221f63a6d418c614cd3cc4b32cc078186a0c2
SungU Kang <20sunrin129@sunrint.hs.kr>
2021-09-21 01:28:18 +0900

    Revert "remove console.log"
    This reverts commit ac2d6a0e653db03fb5b89236b86c4b198181673b.

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -24,7 +24,7 @@ export function tokenize(chars) {
         const e = chars[i];
 
         switch (e) {
-            case '(':thfw
+            case '(':
                 saveAcc();
                 tokens.push(e);
                 clearAcc();
